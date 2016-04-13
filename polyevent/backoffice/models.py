cdc4cb2c938fb6fa5c7f227fa7ea84bb12faf172
Amadeous <juju25_09@hotmail.com>
2016-04-13 21:48:49 +0200

    Models for Event and Activity
    Creation of Models for Event and Activity (the used modules were add in
    requirement.txt and settings.py

--- polyevent/backoffice/models.py
+++ polyevent/backoffice/models.py
@@ -1,5 +1,31 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from location_field.models.plain import PlainLocationField
 
-# Create your models here.
+# For location, a plugin might be found to select places with adress instead of GPS
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    manager = models.ForeignKey(User)
+    description = models.TextField()
+    location = PlainLocationField(based_fields=['city'], zoom=7)
+    date_begin = models.DateTimeField()
+    date_end = models.DateTimeField()
+    website = models.URLField()
+    creation_datetime = models.DateTimeField()
+    last_update = models.DateTimeField()
+    isCancelled = models.BooleanField(default = false)
+
+class Activity(models.Model):
+    event = models.ForeignKey(Event)
+    name = models.CharField(max_length=200)
+    manager = models.ForeignKey(User)
+    description = models.TextField()
+    location = PlainLocationField(based_fields=['city'], zoom=7)
+    date_begin = models.DateTimeField()
+    date_end = models.DateTimeField()
+    website = models.URLField()
+    creation_datetime = models.DateTimeField()
+    last_update = models.DateTimeField()
+    isCancelled = models.BooleanField(default = false)
