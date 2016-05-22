90d641ecab5a3a72a02a504fb54a857012476275
Amadeous <juju25_09@hotmail.com>
2016-05-22 19:54:36 +0200

    Add Tequila auth
    Add an adapted version of Tequila auth for PolyEvent

--- polyevent/polyevent/settings.py
+++ polyevent/polyevent/settings.py
@@ -119,3 +119,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#####################
+# Project specifics #
+#####################
+
+AUTHENTICATION_BACKENDS = ('app.tequila.Backend', 'django.contrib.auth.backends.ModelBackend',)
+
+TEQUILA_SERVER = 'https://tequila.epfl.ch'  # Url of tequila server
+TEQUILA_SERVICE = 'PolyEvent'  # Title used in tequila
+TEQUILA_AUTOCREATE = True  # Auto create users ?
+TEQUILA_FAILURE = '/'  # Where to redirect user if there is a problem
+
+LOGIN_URL = 'app.tequila.login'
+LOGIN_REDIRECT_URL = '/'
